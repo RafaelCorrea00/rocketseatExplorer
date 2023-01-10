7266c89ed94bf53bc3bf6c84a230869e106ee444
Rafael Correa <contato.mundustech@gmail.com>
2023-01-10 14:48:09 -0300

    Commit Github Favorites

---
+++ 18-projeto-github-favorites/js/GithubUser.js
@@ -0,0 +1,16 @@
+// nova classe para pegar a API do Github
+export class GithubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint)
+            .then(data => data.json())
+            .then(({ login, name, public_repos, followers }) => (
+                {
+                    login,
+                    name,
+                    public_repos,
+                    followers
+                }))
+    }
+}
---
+++ 18-projeto-github-favorites/readme.md
@@ -0,0 +1,5 @@
+# Projeto 18 - Github Favorites
+
+* Décimo oitavo projeto desenvolvido no Curso Explorer da Rocketseat
+* Aprendendo conceitos como criar tabelas em HTML, acessibilidade sr-only, classes e herança no JavaScript, criando HTML com JavaScript, imutabilidade para trabalhar com dados da aplicação, localStorage, métodos estáticos e promessas, promessas com Async Await, cuidando do fluxo da aplicação com try, catch e throw
+
